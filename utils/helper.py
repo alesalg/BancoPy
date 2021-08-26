d507e1634f215cab7b05eb247b6fb92070948bc3
Alesalg <allesalg@gmail.com>
2021-08-26 11:45:44 -0300

    implementing classes finish

--- utils/helper.py
+++ utils/helper.py
@@ -6,7 +6,7 @@ def date_para_str(data: date) -> str:
     return data.strftime('%d/%m/%Y')
 
 def str_para_date(data: str) -> date:
-    return datetime.strftime(data, '%d/%m/%Y')
+    return datetime.strptime(data, '%d/%m/%Y')
 
 def formata_float_str_moeda(valor: float ) -> str:
     return f'R$ {valor:,.2f}'
